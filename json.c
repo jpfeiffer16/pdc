b04f3f6a3fa0ca2329786e0bc2dbf710c9bb8012
jpfeiffer16 <joepfeiffer16@gmail.com>
2024-03-23 11:14:45 -0400

    a little bit of parser progres

--- json.c
+++ json.c
@@ -2,53 +2,67 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 /*
   RFC: https://datatracker.ietf.org/doc/html/rfc8259#section-2
 */
 
+#define MAX_JSON_ERROR_SIZE 512
+
 typedef enum {
-  OBJECT,
-  ARRAY,
-  NUMBER,
-  STRING,
-  BOOL
+  JSON_UNKOWN,
+  JSON_ERROR,
+  JSON_OBJECT,
+  JSON_ARRAY,
+  JSON_NUMBER,
+  JSON_STRING,
+  JSON_BOOL,
+  JSON_NULL
 } node_type;
 
-/* typedef struct { */
-/*   node_type type; */
-/*  */
-/*   // Value accessors */
-/*   char **str; */
-/*   int *num; */
-/*   short *bool; */
-/*  */
-/*   node *children; */
-/* } node; */
+char* get_ast_node_type_name(node_type token) {
+  switch (token) {
+    case JSON_UNKOWN: return "JSON_UNKOWN";
+    case JSON_ERROR:  return "JSON_ERROR";
+    case JSON_OBJECT: return "JSON_OBJECT";
+    case JSON_ARRAY:  return "JSON_ARRAY";
+    case JSON_NUMBER: return "JSON_NUMBER";
+    case JSON_STRING: return "JSON_STRING";
+    case JSON_BOOL:   return "JSON_BOOL";
+    case JSON_NULL:   return "JSON_NULL";
+    default:          return "UNKNOWN!";
+  }
+}
+
+typedef struct node {
+  node_type type;
+  struct node *children;
+} node;
 
 typedef enum {
-  NONE,
-  LBRACE,
-  RBRACE,
-  LBRACKET,
-  RBRACKET,
-  IDENTIFIER,
-  STR,
-  COLON,
-  COMMA
+  TK_NONE,
+  TK_LBRACE,
+  TK_RBRACE,
+  TK_LBRACKET,
+  TK_RBRACKET,
+  TK_IDENTIFIER,
+  TK_STRING,
+  TK_COLON,
+  TK_COMMA
 } token_type;
 
 char* get_token_type_name(token_type token) {
   switch (token) {
-    case NONE:       return "NONE";
-    case LBRACE:     return "LBRACE";
-    case RBRACE:     return "RBRACE";
-    case LBRACKET:   return "LBRACKET";
-    case RBRACKET:   return "RBRACKET";
-    case IDENTIFIER: return "IDENTIFIER";
-    case STR:        return "STR";
-    case COLON:      return "COLON";
-    case COMMA:      return "COMMA";
-    default:         return "UNKNOWN!";
+    case TK_NONE:       return "NONE";
+    case TK_LBRACE:     return "LBRACE";
+    case TK_RBRACE:     return "RBRACE";
+    case TK_LBRACKET:   return "LBRACKET";
+    case TK_RBRACKET:   return "RBRACKET";
+    case TK_IDENTIFIER: return "IDENTIFIER";
+    case TK_STRING:     return "STR";
+    case TK_COLON:      return "COLON";
+    case TK_COMMA:      return "COMMA";
+    default:            return "UNKNOWN!";
   }
 }
 
@@ -107,44 +121,44 @@ tokenize_result tokenize(char *buf, long buf_len, int idx) {
     switch (ch) {
       case '{': {
         token_node *tk_l = new_token(tlist);
-        token tk = { .type = LBRACE, .value = "{", .line = line, .column = idx - last_newline };
+        token tk = { .type = TK_LBRACE, .value = "{", .line = line, .column = idx - last_newline };
         tk_l->token = tk;
         break;
       }
       case '}': {
         token_node *tk_l = new_token(tlist);
-        token tk = { .type = RBRACE, .value = "}", .line = line, .column = idx - last_newline };
+        token tk = { .type = TK_RBRACE, .value = "}", .line = line, .column = idx - last_newline };
         tk_l->token = tk;
         break;
       }
       case '[': {
         token_node *tk_l = new_token(tlist);
-        token tk = { .type = LBRACKET, .value = "[", .line = line, .column = idx - last_newline };
+        token tk = { .type = TK_LBRACKET, .value = "[", .line = line, .column = idx - last_newline };
         tk_l->token = tk;
         break;
       }
       case ']': {
         token_node *tk_l = new_token(tlist);
-        token tk = { .type = RBRACKET, .value = "]", .line = line, .column = idx - last_newline };
+        token tk = { .type = TK_RBRACKET, .value = "]", .line = line, .column = idx - last_newline };
         tk_l->token = tk;
         break;
       }
       case ',': {
         token_node *tk_l = new_token(tlist);
-        token tk = { .type = COMMA, .value = ",", .line = line, .column = idx - last_newline };
+        token tk = { .type = TK_COMMA, .value = ",", .line = line, .column = idx - last_newline };
         tk_l->token = tk;
         break;
       }
       case ':': {
         token_node *tk_l = new_token(tlist);
-        token tk = { .type = COLON, .value = ":", .line = line, .column = idx - last_newline };
+        token tk = { .type = TK_COLON, .value = ":", .line = line, .column = idx - last_newline };
         tk_l->token = tk;
         break;
       }
       case '"': {
         token_node *tk_l = new_token(tlist);
         int begin = idx;
-        token tk = { .type = STR, .line = line, .column = idx - last_newline };
+        token tk = { .type = TK_STRING, .line = line, .column = idx - last_newline };
         while (buf[++idx] != '"') {
           if (buf[idx] == '\\') {
             idx++;
@@ -172,7 +186,7 @@ tokenize_result tokenize(char *buf, long buf_len, int idx) {
       default: {
         if (ch > 32 && ch < 127) {
           token_node *tk_l = new_token(tlist);
-          token tk = { .type = IDENTIFIER, .line = line, .column = idx - last_newline };
+          token tk = { .type = TK_IDENTIFIER, .line = line, .column = idx - last_newline };
           int begin = idx;
           while (buf[++idx] != '{'
               && buf[idx]   != '}'
@@ -213,7 +227,89 @@ void print_list(token_list *list) {
   }
 }
 
-void parse(token_list tokens) {
+typedef struct {
+  char *error;
+  node ast_node;
+  token_node *last_node;
+} ast_result;
+
+/* int next(token_type, ); */
+
+ast_result parse_node(token_node *lexer_node) { 
+  token tk = lexer_node->token;
+  ast_result res = { .error = NULL };
+  node n = { };
+  switch(tk.type) {
+    case TK_STRING: {
+      n.type = JSON_STRING;
+      break;
+    }
+    case TK_IDENTIFIER: {
+      if (strcmp(tk.value, "null")) {
+        n.type = JSON_NULL;
+        break;
+      }
+      if (strcmp(tk.value, "false")) {
+        n.type = JSON_BOOL;
+        /* n.value = false; */
+        break;
+      }
+      if (strcmp(tk.value, "true")) {
+        n.type = JSON_BOOL;
+        /* n.value = true; */
+        break;
+      }
+    }
+    case TK_LBRACE: {
+      n.type = JSON_OBJECT;
+      int nest_level = 1;
+      while (nest_level) {
+        lexer_node = lexer_node->next;
+
+        switch (lexer_node->token.type) {
+          case TK_RBRACE: {
+            nest_level--;
+            break;
+          }
+          case TK_STRING: {
+            if (lexer_node->next->token.type != TK_COLON) {
+              n.type = JSON_ERROR;
+              res.error = malloc(MAX_JSON_ERROR_SIZE);
+              res.error = sprintf(
+                res.error,
+                "Error parsing object. Colon expected: %d, %d",
+                lexer_node->next->token.line,
+                lexer_node->next->token.column);
+              return res;
+            }
+            parse_node(lexer_node->next);
+            res.ast_node = n;
+            return res;
+          }
+          default: {
+            n.type = JSON_ERROR;
+            res.error = malloc(MAX_JSON_ERROR_SIZE);
+            snprintf(
+              res.error,
+              MAX_JSON_ERROR_SIZE,
+              "Error parsing object: %d, %d",
+              lexer_node->token.line,
+              lexer_node->token.column);
+            res.ast_node = n;
+            return res;
+          }
+        }
+      }
+    }
+  }
+
+  res.ast_node = n;
+  return res;
+};
+
+void parse(token_list *tokens) {
+  ast_result res = parse_node(tokens->tail);
+  printf("AST: %s\n", get_ast_node_type_name(res.ast_node.type));
 }
 
 
@@ -249,10 +345,12 @@ int main(int argv, char **argc) {
   buffer[count] = '\0';
 
   tokenize_result res = tokenize(buffer, input_size, 0);
-  if (res.status == SUCCESS) {
-    print_list(res.tokens);
-  } else {
+  if (res.status != SUCCESS) {
     fprintf(stderr, "Error parsing json:\n");
     fprintf(stderr, "%s", res.error);
+    exit(1);
   }
+
+  print_list(res.tokens);
+  parse(res.tokens);
 }
